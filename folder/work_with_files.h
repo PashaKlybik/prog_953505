10900e3e690679e3d962a5804a96e665a5c49b74
QwertyForcee <62138442+QwertyForcee@users.noreply.github.com>
2020-05-24 20:18:21 +0300

    Update work_with_files.h

--- folder/work_with_files.h
+++ folder/work_with_files.h
@@ -8,4 +8,6 @@ extern void ShowAllFromFile(List* list,FILE* fPtr);
 extern void CreateFileWithFlights();
 extern void LoadFromFileToList(List* list);
 extern void LoadToFile(Flight* flight);
+extern void DeleteFromFile(int numinlist);
+extern int FItemsGetCount();
 #endif // !WORK_WITH_FILES_H
